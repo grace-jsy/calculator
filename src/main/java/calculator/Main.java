708204c5100c692203139bbabb0cafe6b8d0a4e7
grace-jsy <sseo.oee@gmail.com>
2025-07-12 21:23:42 +0900

    Call a method

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -5,12 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner numbers = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please Enter the numbers and symbols to be calculated(ex. 3 + 5): ");
         
-        int intNumber = numbers.nextInt();
+        int inputNumber1 = scanner.nextInt();
+        char symbol = scanner.next().charAt(0);
+        int inputNumber2 = scanner.nextInt();
 
         Calculator calculator = new Calculator();
+
+        int add = calculator.add(inputNumber1, inputNumber2);
+        System.out.println("Sum: " + add);
+
+        int subtract = calculator.subtract(inputNumber1, inputNumber2);
+        System.out.println("Sum: " + subtract);
+
+        int multiply = calculator.multiply(inputNumber1, inputNumber2);
+        System.out.println("Sum: " + multiply);
+
+        int divide = calculator.multiply(inputNumber1, inputNumber2);
+        System.out.println("Sum: " + divide);
+
+
+
+
         
         
         
