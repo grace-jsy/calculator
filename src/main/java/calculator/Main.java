c3e018004c8c85e7d6b2597f83a5d2dad6db67ef
grace-jsy <sseo.oee@gmail.com>
2025-07-12 20:53:16 +0900

    basic setting

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is test!!!!");
+
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Please Enter the numbers and symbols to be calculated(ex. 3 + 5): ");
+        
+        int intNumber = numbers.nextInt();
+
+        Calculator calculator = new Calculator();
+        
+        
+        
+        
+        
+        
     }
 }
