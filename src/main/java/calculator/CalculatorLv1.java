0a5c21f6c9aa836bac31d3d05adbca23c9a71cee
grace-jsy <sseo.oee@gmail.com>
2025-07-13 17:10:46 +0900

    edited error

--- src/main/java/calculator/CalculatorLv1.java
+++ src/main/java/calculator/CalculatorLv1.java
@@ -7,18 +7,17 @@ public class CalculatorLv1 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please Enter the number1: ");
-        int inputNum1 = scanner.nextInt();
-        System.out.println("Please Enter the symbol: ");
-        char symbol = scanner.next().charAt(0);
-        System.out.println("Please Enter the number2: ");
-        int inputNum2 = scanner.nextInt();
-        char yesNo = scanner.next().charAt(0);
-
-        int result;
-
+        char yesNo;
 
         do {
+            System.out.print("Please Enter the number1: ");
+            int inputNum1 = scanner.nextInt();
+            System.out.print("Please Enter the symbol: ");
+            char symbol = scanner.next().charAt(0);
+            System.out.print("Please Enter the number2: ");
+            int inputNum2 = scanner.nextInt();
+
+            int result;
 
             if (symbol == '+') {
                 result = inputNum1 + inputNum2;
