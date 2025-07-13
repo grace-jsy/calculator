023c60d918206a731a376c0625d9918d18a321d7
grace-jsy <sseo.oee@gmail.com>
2025-07-13 16:57:55 +0900

    Lv1 added loops

--- src/main/java/calculator/CalculatorLv1.java
+++ src/main/java/calculator/CalculatorLv1.java
@@ -13,24 +13,32 @@ public class CalculatorLv1 {
         char symbol = scanner.next().charAt(0);
         System.out.println("Please Enter the number2: ");
         int inputNum2 = scanner.nextInt();
+        char yesNo = scanner.next().charAt(0);
 
         int result;
 
 
+        do {
 
-        if (symbol == '+') {
-            result = inputNum1 + inputNum2;
-            System.out.println("result: " + result);
-        } else if (symbol == '-') {
-            result = inputNum1 - inputNum2;
-            System.out.println("result = " + result);
-        } else if (symbol == '*') {
-            result = inputNum1 * inputNum2;
-            System.out.println("result = " + result);
-        } else if (symbol == '/') {
-            result = inputNum1 / inputNum2;
-            System.out.println("result = " + result);
-        }
+            if (symbol == '+') {
+                result = inputNum1 + inputNum2;
+                System.out.println("result: " + result);
+            } else if (symbol == '-') {
+                result = inputNum1 - inputNum2;
+                System.out.println("result = " + result);
+            } else if (symbol == '*') {
+                result = inputNum1 * inputNum2;
+                System.out.println("result = " + result);
+            } else if (symbol == '/') {
+                result = inputNum1 / inputNum2;
+                System.out.println("result = " + result);
+            }
+
+            System.out.println("Would you like to calculate again?(y/n) ");
+            yesNo = scanner.next().charAt(0);
+
+        } while (yesNo == 'y');
+
 
 
 
