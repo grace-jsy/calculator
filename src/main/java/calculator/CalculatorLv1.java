b595fca43284b845abaa2abbb7c1bc053ef00cbb
grace-jsy <sseo.oee@gmail.com>
2025-07-13 19:46:11 +0900

    i don't know where it should be corrected

--- src/main/java/calculator/CalculatorLv1.java
+++ src/main/java/calculator/CalculatorLv1.java
@@ -29,22 +29,24 @@ public class CalculatorLv1 {
                 result = inputNum1 * inputNum2;
                 System.out.println("result = " + result);
             } else if (symbol == '/') {
-                result = inputNum1 / inputNum2;
+                do {
+                    System.out.println("Please Enter the number2: ");
+                    inputNum2 = scanner.nextInt();
+                    if (inputNum2 == 0) {
+                        System.out.println("You can't be divided by ZERO");
+                    } else if (inputNum2 != 0) {
+                        result = inputNum1 / inputNum2;
+                    }
+                } while (inputNum2 != 0);
+
+            } else {
                 System.out.println("result = " + result);
+                System.out.println("Enter 'exit' to quit. Otherwise, it will continue.");
+                exit = scanner.next();
             }
 
-            System.out.println("Do you want to continue? ( : ");
-            exit = scanner.next();
-
-        } while (!exit.equals("exit"));
 
 
+    } while (!exit.equals("exit"));
 
-
-
-
-
-
-
-    }
 }
