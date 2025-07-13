c730077adc9cae394b5d0d2571f581d6de098271
grace-jsy <sseo.oee@gmail.com>
2025-07-13 16:41:22 +0900

    Lv1 basics

--- src/main/java/calculator/CalculatorLv1.java
+++ src/main/java/calculator/CalculatorLv1.java
@@ -1,7 +1,42 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class CalculatorLv1 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please Enter the number1: ");
+        int inputNum1 = scanner.nextInt();
+        System.out.println("Please Enter the symbol: ");
+        char symbol = scanner.next().charAt(0);
+        System.out.println("Please Enter the number2: ");
+        int inputNum2 = scanner.nextInt();
+
+        int result;
+
+
+
+        if (symbol == '+') {
+            result = inputNum1 + inputNum2;
+            System.out.println("result: " + result);
+        } else if (symbol == '-') {
+            result = inputNum1 - inputNum2;
+            System.out.println("result = " + result);
+        } else if (symbol == '*') {
+            result = inputNum1 * inputNum2;
+            System.out.println("result = " + result);
+        } else if (symbol == '/') {
+            result = inputNum1 / inputNum2;
+            System.out.println("result = " + result);
+        }
+
+
+
+
+
+
 
 
     }
