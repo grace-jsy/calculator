886ea0cae1f21a484afa454943d9a0477fd686a2
grace-jsy <sseo.oee@gmail.com>
2025-07-14 10:27:15 +0900

    Lv2 edited

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        char yesNo;
+        String exit = "";
 
         do {
 
@@ -15,32 +15,56 @@ public class Main {
 
             System.out.print("Please Enter the number1: ");
             int inputNumber1 = scanner.nextInt();
-            System.out.print("Please Enter the symbol: ");
-            char symbol = scanner.next().charAt(0);
-            System.out.print("Please Enter the number2: ");
-            int inputNumber2 = scanner.nextInt();
-
-            if (symbol == '+') {
-                int add = calculator.add(inputNumber1, inputNumber2);
-                System.out.println("Result: " + add);
-            } else if (symbol == '-') {
-                int subtract = calculator.subtract(inputNumber1, inputNumber2);
-                System.out.println("Result: " + subtract);
-            } else if (symbol == '*') {
-                int multiply = calculator.multiply(inputNumber1, inputNumber2);
-                System.out.println("Result: " + multiply);
-            } else if (symbol == '/') {
-                int divide = calculator.divide(inputNumber1, inputNumber2);
-                System.out.println("Result: " + divide);
+            if (inputNumber1 < 0) {
+                System.out.println("Only positive integers are allowed.");
+                continue;
             }
+                System.out.print("Please Enter the symbol: ");
+                char symbol = scanner.next().charAt(0);
+
+                System.out.print("Please Enter the number2: ");
+                int inputNumber2 = scanner.nextInt();
+                if (inputNumber2 < 0) {
+                    System.out.println("Only positive integers are allowed.");
+                    continue;
+                }
+
+                int result = 0;
 
 
-            System.out.println("Type 'exit' to quit, or press any other key to continue.");
+                if (symbol == '+') {
+                    int add = calculator.add(inputNumber1, inputNumber2);
+                    System.out.println("Result: " + add);
+                } else if (symbol == '-') {
+                    int subtract = calculator.subtract(inputNumber1, inputNumber2);
+                    System.out.println("Result: " + subtract);
+                } else if (symbol == '*') {
+                    int multiply = calculator.multiply(inputNumber1, inputNumber2);
+                    System.out.println("Result: " + multiply);
+                } else if (symbol == '/') {
 
-            yesNo = scanner.next().charAt(0);
+                    do {
+                        if (inputNumber2 == 0) {
+                            System.out.println("It can't be divided by ZERO");
+                            System.out.print("Please Enter the number2: ");
+                            inputNumber2 = scanner.nextInt();
+                        } else if (inputNumber2 != 0) {
+                            result = inputNumber1 / inputNumber2;
+                            System.out.println("result = " + result);
+                            break;
+                        }
+                    } while (inputNumber2 != 0);
 
-        } while(yesNo == 'y');
+                }
 
-        
+
+                System.out.println("Type 'exit' to quit, or press any other key to continue.");
+                scanner.nextLine();
+                exit = scanner.nextLine();
+
+        } while (!exit.equals("exit"));
     }
+
 }
+
+
