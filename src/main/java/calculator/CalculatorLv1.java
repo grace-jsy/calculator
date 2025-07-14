eadee2d35e494756616c8ec974038066ca888b04
grace-jsy <sseo.oee@gmail.com>
2025-07-14 09:39:13 +0900

    complete

--- src/main/java/calculator/CalculatorLv1.java
+++ src/main/java/calculator/CalculatorLv1.java
@@ -6,18 +6,29 @@ public class CalculatorLv1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         String exit = "";
 
         do {
             System.out.print("Please Enter the number1: ");
             int inputNum1 = scanner.nextInt();
-            System.out.print("Please Enter the symbol: ");
+
+            if (inputNum1 < 0) {
+                System.out.println("0이상만써");
+                continue;
+            }
+
+            System.out.print("Please Enter the symbol(+, -, *, /): ");
             char symbol = scanner.next().charAt(0);
+
             System.out.print("Please Enter the number2: ");
             int inputNum2 = scanner.nextInt();
 
-            int result;
+            if (inputNum2 < 0) {
+                System.out.println("0이상만써");
+                continue;
+            }
+
+            int result = 0;
 
             if (symbol == '+') {
                 result = inputNum1 + inputNum2;
@@ -30,23 +41,23 @@ public class CalculatorLv1 {
                 System.out.println("result = " + result);
             } else if (symbol == '/') {
                 do {
-                    System.out.println("Please Enter the number2: ");
-                    inputNum2 = scanner.nextInt();
                     if (inputNum2 == 0) {
-                        System.out.println("You can't be divided by ZERO");
+                        System.out.println("It can't be divided by ZERO");
+                        System.out.print("Please Enter the number2: ");
+                        inputNum2 = scanner.nextInt();
                     } else if (inputNum2 != 0) {
                         result = inputNum1 / inputNum2;
+                        System.out.println("result = " + result);
+                        break;
                     }
                 } while (inputNum2 != 0);
 
-            } else {
-                System.out.println("result = " + result);
-                System.out.println("Enter 'exit' to quit. Otherwise, it will continue.");
-                exit = scanner.next();
             }
 
+            System.out.println("Enter 'exit' to quit. Otherwise, it will continue.");
+            exit = scanner.next();
 
+        } while (!exit.equals("exit")) ;
 
-    } while (!exit.equals("exit"));
-
+    }
 }
